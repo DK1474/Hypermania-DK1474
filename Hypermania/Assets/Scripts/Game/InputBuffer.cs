e99634334a19e8a3c0a52338d6c8383490e79a47
Glitcher01 <lokesht20077@gmail.com>
2026-01-26 08:45:03 -0500

    Finished basics of input buffer

--- Hypermania/Assets/Scripts/Game/InputBuffer.cs
+++ Hypermania/Assets/Scripts/Game/InputBuffer.cs
@@ -1,3 +1,4 @@
+using System;
 using Game.Sim;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,18 +8,25 @@ namespace Game
     // TODO: implement with actual buffer, allow customization
     public class InputBuffer
     {
-        private InputFlags _curInput;
+        private InputFlags[] buffer = new InputFlags[16];
+        private int frontIdx = 0;
+        private int size = 0;
 
         public void Saturate()
         {
             InputFlags input = InputFlags.None;
 
+            // Movement
             if (Keyboard.current.aKey.isPressed)
                 input |= InputFlags.Left;
             if (Keyboard.current.dKey.isPressed)
                 input |= InputFlags.Right;
             if (Keyboard.current.wKey.isPressed)
                 input |= InputFlags.Up;
+            if (Keyboard.current.sKey.isPressed)
+                input |= InputFlags.Down;
+
+            // Attacks
             if (Keyboard.current.jKey.isPressed)
                 input |= InputFlags.LightAttack;
             if (Keyboard.current.kKey.isPressed)
@@ -26,6 +34,7 @@ namespace Game
             if (Keyboard.current.lKey.isPressed)
                 input |= InputFlags.SuperAttack;
 
+            // Mania Keys
             if (Keyboard.current.aKey.isPressed)
                 input |= InputFlags.Mania5;
             if (Keyboard.current.sKey.isPressed)
@@ -39,13 +48,33 @@ namespace Game
             if (Keyboard.current.lKey.isPressed)
                 input |= InputFlags.Mania6;
 
-            _curInput |= input;
+
+            buffer[(frontIdx + size) % buffer.Length] = input;
+            if (size < buffer.Length)
+            {
+                size++;
+            } else
+            {
+                frontIdx = (frontIdx + 1) % buffer.Length;
+            }
         }
 
         public GameInput Consume()
         {
-            GameInput res = new GameInput(_curInput);
-            _curInput = InputFlags.None;
+            GameInput res;
+            if (size == 0)
+            {
+                res = new GameInput(InputFlags.None);
+            }
+            else
+            {
+                res = new GameInput(buffer[frontIdx]);
+                Debug.Log(buffer[frontIdx]);
+                frontIdx = (frontIdx + 1) % buffer.Length;
+                size--;
+            }
+            Debug.Log(string.Join(" : ", buffer));
+
             return res;
         }
     }
